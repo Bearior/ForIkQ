130f47574e6b253d2c0f03acf4652bca41e5fad1
Khunasin Sooksri <86812723+Bearior@users.noreply.github.com>
2023-02-10 17:34:59 +0700

    e

--- src/components/Pages/UploadData.js
+++ src/components/Pages/UploadData.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import app from "../../firebase";
 import firebase from "firebase/app";
 import "firebase/firestore";
 
@@ -6,11 +7,26 @@ const Upload = () => {
   const [PH1, setPH1] = useState("");
   const [PH2, setPH2] = useState("");
   const [PH3, setPH3] = useState("");
+  const [file, setFile] = useState(null);
+  const [base64, setBase64] = useState(null);
+
   console.log(PH1);
   console.log(PH2);
 
+  const handleChange = (e) => {
+    const file = e.target.files[0];
+    setFile(file);
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => {
+        const base64 = fileReader.result;
+        setBase64(base64);
+        console.log(base64)
+    };
+};
+
   const Submithandle = () => {
-    const db = firebase.firestore();
+    const db = app.firestore();
     setPH1("");
     setPH2("");
     setPH3("");
@@ -22,7 +38,8 @@ const Upload = () => {
         fileRef.set({ 
         PH1 : PH1,
         PH2 : PH2,
-        PH3 : PH3
+        PH3 : PH3,
+        File : base64
       });
       
       alert("Data uploaded");
@@ -50,6 +67,12 @@ const Upload = () => {
         <label> PH3
            <input type="text" 
            onChange={(e) => setPH3(e.target.value)}
+           value={PH3}/>
+        </label><br></br>
+
+        <label> Picture upload
+           <input type="file" 
+           onChange={handleChange}
            value={PH3}/>
         </label><br></br>
         <button onClick={Submithandle}> Submit</button>
--- src/firebase.js
+++ src/firebase.js
@@ -3,8 +3,14 @@ import "firebase/auth";
 import 'firebase/storage';
 
 const app = firebase.initializeApp({
-    // your Credentials URS
-  });
+  apiKey: "AIzaSyCOLdComqETNmh7Gy9-ORdATTY0vaxzEJk",
+  authDomain: "eipcadb-2.firebaseapp.com",
+  projectId: "eipcadb-2",
+  storageBucket: "eipcadb-2.appspot.com",
+  messagingSenderId: "920418332400",
+  appId: "1:920418332400:web:5509e0b7203338741ddc35",
+  measurementId: "G-8QJE1JV5XF"
+});
 
 
 export default app;
